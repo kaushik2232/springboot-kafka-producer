6233db6994700d78c130454feb2c922042e5d91e
kaushik.satya77@gmail.com <kaushik.satya77@gmail.com>
2019-09-10 23:03:47 -0500

    Kafka-producer

---
+++ src/main/java/com/spring/kafka/springbootkafka/Resource/EmployeeResource.java
@@ -0,0 +1,26 @@
+package com.spring.kafka.springbootkafka.Resource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("kafka")
+public class EmployeeResource {
+    
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    private static final String TOPIC = "Kafka_Example";
+
+    @GetMapping("/publish/{message}")
+    public String post(@PathVariable("message") final String message) {
+
+        kafkaTemplate.send(TOPIC, message);
+
+        return "Published successfully";
+    }
+}
